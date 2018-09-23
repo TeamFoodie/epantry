f9022aa5582c51ae9744eb1e42665ff8b28c3800
setavita <42175076+setavita@users.noreply.github.com>
2018-09-23 21:47:22 +1200

    linked shopping list and add new recipe to landing page

--- epantry/app/src/main/java/com/example/setavita/epantry/activity/LandingPageActivity.java
+++ epantry/app/src/main/java/com/example/setavita/epantry/activity/LandingPageActivity.java
@@ -12,6 +12,8 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
 
     private ImageView restock;
     private ImageView search;
+    private ImageView uploadRecipe;
+    private ImageView shoppingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,12 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
 
         restock = (ImageView) findViewById(R.id.myPantry);
         restock.setOnClickListener(this);
-        search = (ImageView) findViewById(R.id.searchRecipe);
+        search = (ImageView) findViewById(R.id.find_recipe);
         search.setOnClickListener(this);
+        uploadRecipe = (ImageView) findViewById(R.id.newRecipe);
+        uploadRecipe.setOnClickListener(this);
+        shoppingList = (ImageView) findViewById(R.id.shop_list);
+        shoppingList.setOnClickListener(this);
 
     }
 
@@ -30,8 +36,16 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
         if (v.getId() == R.id.myPantry) {
             Intent intent = new Intent(LandingPageActivity.this, ViewPantryActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.searchRecipe){
+        }else if (v.getId() == R.id.find_recipe){
             Intent intent = new Intent(LandingPageActivity.this, ViewAllRecipesActivity.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.newRecipe){
+            Intent intent = new Intent(LandingPageActivity.this, MakeFoodActivity.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.shop_list){
+            Intent intent = new Intent(LandingPageActivity.this, FoodMaterialListActivity.class);
             startActivity(intent);
         }
     }
