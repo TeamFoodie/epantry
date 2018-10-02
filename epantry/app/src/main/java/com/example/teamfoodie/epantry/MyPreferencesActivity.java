fb5d097d858ac3e049013842ea0942b6ae29aa6a
Charizze <austin.charizze@gmail.com>
2018-10-03 00:31:41 +1300

    Base UI of MyPreferences class, navigation drawer now links to MyPreferences activity screen

--- epantry/app/src/main/java/com/example/teamfoodie/epantry/MyPreferencesActivity.java
+++ epantry/app/src/main/java/com/example/teamfoodie/epantry/MyPreferencesActivity.java
@@ -1,9 +1,20 @@
-//for String : arrayList { new Checkbox(); }
+//TODO:
+//1) get Title for each ingredient in pantry
+//          - for String : arrayList { new Checkbox(); }
+//2) checkboxes:
+//      * if ticked:
+//              - calculateThreshold()
+//              - set each ingredient's threshold to "threshold"
+//              - boolean priority = true
+//      * if priority == true
+//              - ... send alert to phone every "alertDays"
+//3) add when back button clicked...
 
 package com.example.teamfoodie.epantry;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MyPreferencesActivity extends AppCompatActivity{
 
@@ -11,6 +22,12 @@ public class MyPreferencesActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_preferences);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("My Preferences"); // for set actionbar title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // for add back arrow in action bar
 
         System.out.println("Loaded class");
     }
